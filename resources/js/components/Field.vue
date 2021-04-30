b10a84c560a52e5143e5dea16b331a2619a04fab
Gautier <gautier.deleglise@gmail.com>
2021-04-30 23:28:02 +0200

    :sparkles: responsiveness

--- resources/js/components/Field.vue
+++ resources/js/components/Field.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mb-6 px-3 w-1/4">
-    <card class="flex flex-col items-center justify-center">
+    <card class="flex flex-col items-center justify-center px-3 py-3">
       <heading class="mb-3">{{ field.name }}</heading>
       <span
         class="w-full text-center whitespace-no-wrap px-2 py-2 uppercase text-xs font-bold border-2"
